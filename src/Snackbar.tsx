dce56387a4bb46d3ef299de9616317cc329e5738
nandobfer <nandobfer@yahoo.com.br>
2023-05-25 16:30:09 -0300

    feat: added anchorOrigin and autoHideDuration props

--- src/Snackbar.tsx
+++ src/Snackbar.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Snackbar as MuiSnackbar, Alert, SnackbarContentProps } from "@mui/material"
+import { Snackbar as MuiSnackbar, Alert, SnackbarContentProps, SnackbarOrigin } from "@mui/material"
 import { useSnackbar } from "./useSnackbar"
 import { SxProps } from "@mui/system"
 
@@ -7,17 +7,19 @@ interface SnackbarProps {
     sx?: SxProps
     contentProps?: Partial<SnackbarContentProps>
     alertSx?: SxProps
+    anchorOrigin?: SnackbarOrigin
+    autoHideDuration: number
 }
 
-export const Snackbar: React.FC<SnackbarProps> = ({ sx, contentProps, alertSx }) => {
+export const Snackbar: React.FC<SnackbarProps> = ({ sx, contentProps, alertSx, anchorOrigin, autoHideDuration }) => {
     const snackbar = useSnackbar()
 
     return (
         <MuiSnackbar
             open={snackbar.open}
-            autoHideDuration={3000}
+            autoHideDuration={autoHideDuration || 3000}
             onClose={() => snackbar.setOpen(false)}
-            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+            anchorOrigin={anchorOrigin || { vertical: "bottom", horizontal: "right" }}
             sx={sx}
             ContentProps={contentProps}
         >
